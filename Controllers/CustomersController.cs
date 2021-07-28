0f3de88addf9a36f83cad14582a63f44e4d642d9
Code-Alexandre <alex.lapres@outlook.com>
2021-07-28 08:36:46 -0400

    rest api for customer portal

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -40,6 +40,63 @@ namespace FactIntervention.Controllers
 
             return customer;
         }
+                [HttpGet("{email}")]
+        public async Task<ActionResult<Customer>> GetCustomer(string email)
+        {
+            var customer = await _context.customers.Include("Buildings.Batteries.Columns.Elevators")
+                                                .Where(c => c.email_of_the_company_contact == email)
+                                                .FirstOrDefaultAsync();  
+
+            // customer = await _context.customers.Include("Buildings.Addresses")
+            //                                     .Where(c => c.cpy_contact_email == email)
+            //                                     .FirstOrDefaultAsync();          
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return customer;
+        }
+                [HttpGet("verify/{email}")]
+        public async Task<ActionResult> VerifyEmail(string email)
+        {
+            var customer = await _context.customers.Include("Buildings.Batteries.Columns.Elevators")
+                                                .Where(c => c.email_of_the_company_contact == email)
+                                                .FirstOrDefaultAsync();            
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<ActionResult<Customer>> PutCustomer(Customer customer)
+        {
+            var customerToUpdate = await _context.customers
+                                                .Where(c => c.email_of_the_company_contact == customer.email_of_the_company_contact)
+                                                .FirstOrDefaultAsync(); 
+
+            if (customerToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            customerToUpdate.compagny_name = customer.compagny_name;
+            customerToUpdate.full_name_of_the_company_contact = customer.full_name_of_the_company_contact;
+            customerToUpdate.company_contact_phone = customer.company_contact_phone;
+            customerToUpdate.full_name_of_service_technical_authority = customer.full_name_of_service_technical_authority;
+            customerToUpdate.technical_authority_phone_for_service = customer.technical_authority_phone_for_service;
+            customerToUpdate.technical_manager_email_for_service = customer.technical_manager_email_for_service;
+            customerToUpdate.company_description = customer.company_description;
+
+            await _context.SaveChangesAsync();
+
+            return customerToUpdate;
+        }
+    
 
         // PUT: api/Customers/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -9,5 +9,11 @@ public class Customer
     public long Id { get; set; }
     public string email_of_the_company_contact { get; set; }
     public string technical_manager_email_for_service { get; set; }
+    public string compagny_name { get; set; }
+    public string full_name_of_the_company_contact { get; set; }
+    public string company_contact_phone { get; set; }
+    public string full_name_of_service_technical_authority { get; set; }
+    public string technical_authority_phone_for_service { get; set; }
+    public string company_description { get; set; }
     
 }
